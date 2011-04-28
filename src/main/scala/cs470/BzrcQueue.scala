eac65a527c68c94b9e509aa621fb3e601862cc81
Tyler Southwick <tylersouthwick@byu.edu>
2011-04-28 10:34:24 -0600

    using thread queue not actors

--- src/main/scala/cs470/BzrcQueue.scala
+++ src/main/scala/cs470/BzrcQueue.scala
@@ -1,48 +1,27 @@
 package cs470
 
 import scala.actors._
-import Actor._
+import java.util.concurrent.{Callable, Executors}
 
 class BzrcQueue(host: String, port: Int) {
 
 	private val LOG = org.apache.log4j.Logger.getLogger(classOf[BzrcQueue])
 
-	private case class ItemAvailable()
-
 	private val con = new BzFlagConnection(host, port)
-	private val callbacks = new java.util.concurrent.ConcurrentLinkedQueue[BzFlagConnection => Unit]
-
+	private val queue = Executors.newSingleThreadExecutor
 
 	def invoke(callback: BzFlagConnection => Unit) {
-		callbacks.add(callback)
-		LOG.debug("sending ItemAvailable")
-		queue ! ItemAvailable
+		queue.execute(new Runnable () {
+			def run() {
+				callback(con)
+			}
+		})
 	}
 
 	def invokeAndWait[T](callback: BzFlagConnection => T): T = {
-		val me = self
-		callbacks.add {
-			con =>
-				me ! callback(con)
-		}
-
-		queue ! ItemAvailable
-		receive {
-			case t: AnyRef => t.asInstanceOf[T]
-			case _ => throw new IllegalStateException
-		}
-	}
-
-	private val queue = actor {
-		loop {
-			react {
-				case ItemAvailable => {
-					LOG.debug("got ItemAvailable")
-					val callback = callbacks.poll
-					callback(con)
-				}
-			}
-		}
+		queue.submit(new Callable[T] {
+			def call() = callback(con)
+		}).get
 	}
 
 }
