f71558adb7e94c74727e45bcf1a8230602d98aa3
Taylor Southwick <twsouthwick@hotmail.com>
2011-04-27 20:01:42 -0600

    Impleneted DummyAgent

--- src/main/scala/cs470/agents/Agent.scala
+++ src/main/scala/cs470/agents/Agent.scala
@@ -2,13 +2,27 @@ package cs470.agents
 
 import cs470.BzrcQueue
 import cs470.domain.MyTank
+import actors._
+import Actor._
 
-abstract class Agent(host : String, port : Int) {
+abstract class Agent(host: String, port: Int) {
   val queue = new BzrcQueue(host, port)
 
   def run
 
-  implicit def tankSpeed(tank : MyTank) = new {
-    def speed(s : Float) = queue.invoke(_.speed(tank.id,s))
+  implicit def tankSpeed(tank: MyTank) = new {
+    def speed(s: Float) = queue.invoke(_.speed(tank.id, s))
+
+    def shoot = queue.invoke(_.shoot((tank.id)))
+  }
+
+  def timeout(milliseconds : => Long)(callback : => Unit) {
+    actor {
+      loop {
+        reactWithin(milliseconds) {
+          case TIMEOUT => callback
+        }
+      }
+    }
   }
 }
--- src/main/scala/cs470/agents/DummyAgent.scala
+++ src/main/scala/cs470/agents/DummyAgent.scala
@@ -1,8 +1,28 @@
 package cs470.agents
 
-class DummyAgent(host : String,port : Int) extends Agent(host,port) {
+import cs470.domain.MyTank
+import scala.actors._
+import Actor._
+
+class DummyAgent(host: String, port: Int) extends Agent(host, port) {
   def run {
-      val tanks = queue.invokeAndWait(_.mytanks)
-      tanks.foreach(tank => tank.speed(1))
+    val tanks = queue.invokeAndWait(_.mytanks)
+    tanks.foreach(moveDummyTanks(_))
   }
+
+  def moveDummyTanks(tank: MyTank) = {
+    //Go forward a bit, then rotate 60 degrees
+    timeout(3000) {
+        tank.speed(1.0f)
+        //wait 3-8 seconds
+        tank.speed(0.0f)
+        //rotate ~60 degrees
+    }
+
+    //Shoot every 1.5-2.5 seconds
+    timeout(2000) {
+      tank.shoot
+    }
+  }
+
 }
