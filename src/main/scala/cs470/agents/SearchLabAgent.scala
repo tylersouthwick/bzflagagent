03c7de4772c696080a481e8ce155d1a696d9f714
Tyler Southwick <tylersouthwick@byu.edu>
2011-05-12 13:43:22 -0600

    added penelized heuristic

--- src/main/scala/cs470/agents/SearchLabAgent.scala
+++ src/main/scala/cs470/agents/SearchLabAgent.scala
@@ -3,6 +3,8 @@ package cs470.agents
 import cs470.movement.search._
 
 class SearchLabAgent(host : String, port : Int) extends Agent(host, port) {
+	val penelized = "true" == System.getProperty("penelized")
+
 	def run() {
 		val team = constants("team")
 
@@ -29,9 +31,31 @@ class SearchLabAgent(host : String, port : Int) extends Agent(host, port) {
 		val title = "Uniform Cost Search"
 	}
 
-	def aStar = new A_StarSearcher with AgentSearcher {
-		val filename = "a_star.gpi"
-		val title = "A* Search"
+	def aStar = {
+		if (penelized) {
+			new LabAStarSearcher {
+				override def h(n: Node) = super.h(n) * p(n)
+
+				def p(n : Node) = {
+					val current = n.parent
+					if (current.nextToOccupied) {
+						if (n.nextToOccupied) {
+							1.5
+						} else {
+							1.1
+						}
+					} else {
+						if (n.nextToOccupied) {
+							1.3
+						} else {
+							1.0
+						}
+					}
+				}
+			}
+		} else {
+			new LabAStarSearcher
+		}
 	}
 
 	def breadthFirst = new BreadthFirstSearcher with AgentSearcher {
@@ -45,6 +69,11 @@ class SearchLabAgent(host : String, port : Int) extends Agent(host, port) {
 		val start = store.tanks(tankId).location
 		val goal = greenFlag
 		val q = queue
+	}
+
+	class LabAStarSearcher extends A_StarSearcher with AgentSearcher {
+		val filename = "a_star.gpi"
+		val title = "A* Search"
 	}
 }
 
