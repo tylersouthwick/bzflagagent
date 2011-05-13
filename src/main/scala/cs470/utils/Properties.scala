0150a5b94dd545ca50f73a2122c285861eb42937
Tyler Southwick <tylersouthwick@byu.edu>
2011-05-13 09:59:41 -0600

    added a property handler for doubles

--- src/main/scala/cs470/utils/Properties.scala
+++ src/main/scala/cs470/utils/Properties.scala
@@ -18,6 +18,7 @@ object Properties {
 	def apply(s : String, default : String) = convert(s, default) (t => t)
 	def apply(s : String, default : Int) = convert(s, default) (Integer.parseInt(_))
 	def apply(s : String, default : Boolean) = convert(s, default) ("true" == _)
+	def apply(s : String, default : Double) = convert(s, default) (java.lang.Double.parseDouble(_))
 
 	private def convert[T](s : String, default : T)(callback : (String) => T) : T = this(s) match {
 		case None => use(s, default)
