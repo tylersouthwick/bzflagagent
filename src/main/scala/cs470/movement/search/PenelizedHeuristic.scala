0dfe985f580fb689c4deaf87ced794956e5279cb
Tyler Southwick <tylersouthwick@byu.edu>
2011-05-13 10:00:08 -0600

    made penelized heuristic read properties

--- src/main/scala/cs470/movement/search/PenelizedHeuristic.scala
+++ src/main/scala/cs470/movement/search/PenelizedHeuristic.scala
@@ -1,20 +1,27 @@
 package cs470.movement.search
 
+import cs470.utils.Properties
+
 trait PenelizedHeuristic extends HeuristicSearcher {
+
+	val nextToNextTo = Properties("penelized.nextToNextTo", 1.5)
+	val nextToAway = Properties("penelized.nextToAway", 1.1)
+	val awayNextTo = Properties("penelized.awayNextTo", 1.3)
+	val awayAway = Properties("penelized.awayAway", 1.0)
 
 	override def h(n: Node) = super.h(n) * {
 		val current = n.parent
 		if (current.nextToOccupied) {
 			if (n.nextToOccupied) {
-				1.5
+				nextToNextTo
 			} else {
-				1.1
+				nextToAway
 			}
 		} else {
 			if (n.nextToOccupied) {
-				1.3
+				awayNextTo
 			} else {
-				1.0
+				awayAway
 			}
 		}
 	}
