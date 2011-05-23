566d10efd80c71b7ebb9a3fd32a427a5b7586e60
Tyler Southwick <tyler@northfuse.net>
2011-05-23 13:05:55 -0600

    added PotentialFieldsMover

---
+++ src/main/scala/cs470/movement/PotentialFieldsMover.scala
@@ -0,0 +1,87 @@
+package cs470.agents
+
+import cs470.domain._
+import Constants._
+import cs470.movement._
+import cs470.utils._
+import Angle._
+import java.lang.Math._
+import cs470.bzrc._
+import cs470.visualization.PFVisualizer
+
+abstract class PotentialFieldsMover(store : DataStore) {
+
+	import PotentialFieldAgent._
+
+	val constants = store.constants
+	val Kp = 1
+	val Kd = 4.5
+	val tol = degree(2).radian
+	val tolv = .1
+	val maxVel: Double = constants("tankangvel")
+	val worldsize: Int = constants("worldsize")
+	val maxMagnitude = 100.0
+	val maxVelocity = 1
+	val team = constants("team")
+	val turningSpeed = 0.6
+	val tank : Tank
+
+	val finder : TankPathFinder
+
+	import RefreshableData.waitForNewData
+
+	def moveAlongPotentialField() {
+		def move(pdVector : => Vector) {
+			//	tank.speed(vector.magnitude / maxMagnitude)
+			//val (angle, time) = tank.moveAngle(vector.angle)
+
+			def pdController(error0: Radian, vector: Vector) {
+				val targetAngle = vector.angle
+				val angle = tank.angle
+				LOG.debug("targetAngle: " + targetAngle.degree)
+				LOG.debug("angle: " + angle.degree)
+				val error = targetAngle - angle
+
+				LOG.debug("error: " + error.degree)
+				val rv = (Kp * error + Kd * (error - error0) / 200);
+				LOG.debug("rv: " + rv)
+				val v = if (rv > maxVel) 1 else rv / maxVel
+				LOG.debug("v: " + v)
+
+				if (abs(error) < tol && abs(v) < tolv) {
+					LOG.debug("Done Turning");
+					tank.setAngularVelocity(0f)
+					val speed = {
+						val m = vector.magnitude
+						LOG.debug("magnitude: " + m)
+						val result = m / 30.0
+						if (result > maxVelocity) {
+							maxVelocity
+						} else {
+							result
+						}
+					}
+					LOG.debug("setting speed: " + speed)
+					tank.speed(speed)
+					waitForNewData()
+				} else {
+					//Agents.LOG.debug("Setting velocity to " + v)
+					tank.setAngularVelocity(v)
+					//slow it down to turn
+					LOG.debug("setting speed: " + turningSpeed)
+					tank.speed(turningSpeed)
+					waitForNewData()
+					pdController(error, pdVector)
+				}
+			}
+
+			pdController(radian(0), pdVector)
+		}
+	}
+
+}
+
+trait TankPathFinder {
+	def path : Vector
+	val color : String
+}
